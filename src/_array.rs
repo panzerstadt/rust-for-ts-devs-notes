32fe3aaaa546c8d8a7fa729bb70871eb4d437550
panzerstadt <tliqun@gmail.com>
2023-10-01 13:38:04 +0900

    tell it to infer by itself

--- src/_array.rs
+++ src/_array.rs
@@ -1,6 +1,6 @@
 pub fn array_rs() {
     let the_list = vec![1, 2, 3];
-    let added: Vec<i32> = the_list.iter().map(|x| x + 1).collect();
+    let added: Vec<_> = the_list.iter().map(|x| x + 1).collect();
 
     println!("{:?}", added);
 }

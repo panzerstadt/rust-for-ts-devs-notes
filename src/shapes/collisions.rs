9868e4ab44b565ac022353395868a1fcbbf187b1
panzerstadt <tliqun@gmail.com>
2023-10-18 21:09:00 +0900

    blanket implementation refactor

--- src/shapes/collisions.rs
+++ src/shapes/collisions.rs
@@ -14,3 +14,55 @@ pub trait Collidable<T> {
         return false;
     }
 }
+
+pub struct PointIter {
+    points: Vec<(f64, f64)>,
+    idx: usize,
+}
+
+impl Iterator for PointIter {
+    type Item = (f64, f64);
+
+    // rusty
+    fn next(&mut self) -> Option<Self::Item> {
+        let idx = self.idx;
+        self.idx += 1;
+
+        return self.points.get(idx).map(|x| *x); // deref means you COPY the data
+                                                 // *x only works if the underlying datatype has the Copy Trait
+    }
+}
+
+// can't implement foreign traits for foreign types
+// impl From<Vec<(f64, f64)>> for f64 {
+impl From<Vec<(f64, f64)>> for PointIter {
+    fn from(points: Vec<(f64, f64)>) -> Self {
+        return PointIter { points, idx: 0 };
+    }
+}
+
+pub trait Points {
+    fn points(&self) -> PointIter;
+}
+
+pub trait Contains {
+    fn contains_point(&self, point: (f64, f64)) -> bool;
+}
+
+// for any T that implements Points and Contains,
+// we will implement Collidable for them
+// T is other, V is self
+impl<Other, Me> Collidable<Other> for Me
+where
+    Other: Points,
+    Me: Contains,
+{
+    fn collide(&self, other: &Other) -> bool {
+        for point in other.points() {
+            if self.contains_point(point) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
--- src/shapes/rect.rs
+++ src/shapes/rect.rs
@@ -1,4 +1,7 @@
-use super::{area::Area, circle::Circle, collisions::Collidable};
+use super::{
+    area::Area,
+    collisions::{Contains, Points},
+};
 use std::fmt::Display;
 
 pub struct Rectangle {
@@ -8,26 +11,21 @@ pub struct Rectangle {
     pub height: f64,
 }
 
-impl Rectangle {
-    pub fn contains_point(&self, (x, y): (f64, f64)) -> bool {
+impl Contains for Rectangle {
+    fn contains_point(&self, (x, y): (f64, f64)) -> bool {
         return self.x <= x && self.x + self.width >= x && self.y <= y && self.y + self.height >= y;
     }
 }
 
-impl Collidable<Rectangle> for Rectangle {
-    fn collide(&self, other: &Rectangle) -> bool {
-        for point in other {
-            if self.contains_point(point) {
-                return true;
-            }
-        }
-        return false;
-    }
-}
-
-impl Collidable<Circle> for Rectangle {
-    fn collide(&self, other: &Circle) -> bool {
-        return self.contains_point((other.x, other.y));
+impl Points for Rectangle {
+    fn points(&self) -> super::collisions::PointIter {
+        return vec![
+            (self.x, self.y),
+            (self.x + self.width, self.y),
+            (self.x, self.y + self.height),
+            (self.x + self.width, self.y + self.height),
+        ]
+        .into();
     }
 }
 
@@ -55,89 +53,5 @@ impl Display for Rectangle {
             "Rectangle({}, {}): {}x{}",
             self.x, self.y, self.width, self.height
         );
-    }
-}
-
-pub struct RectIter {
-    points: Vec<(f64, f64)>,
-    idx: usize,
-}
-
-impl Iterator for RectIter {
-    type Item = (f64, f64);
-
-    // typescripty
-    // fn next(&mut self) -> Option<Self::Item> {
-    //     if self.idx >= self.points.len() {
-    //         return None; // None is the signal to the outside world that you're done with the iterating
-    //     }
-    //     // 1. get current point
-    //     let point = self.points[self.idx];
-    //     // 2. increment index
-    //     self.idx += 1;
-
-    //     return Some(point);
-    // }
-
-    // rusty
-    fn next(&mut self) -> Option<Self::Item> {
-        let idx = self.idx;
-        self.idx += 1;
-
-        return self.points.get(idx).map(|x| *x); // deref means you COPY the data
-                                                 // *x only works if the underlying datatype has the Copy Trait
-    }
-}
-
-// v2 even better refactor, this is the From trait, a built-in trait
-impl From<&Rectangle> for RectIter {
-    fn from(value: &Rectangle) -> Self {
-        return RectIter {
-            points: vec![
-                (value.x, value.y),
-                (value.x + value.width, value.y),
-                (value.x, value.y + value.height),
-                (value.x + value.width, value.y + value.height),
-            ],
-            idx: 0,
-        };
-    }
-}
-
-// v1 refactor to reduce duplication
-// impl RectIter {
-//     // can be called anything you want, not just new()
-//     fn new(rect: &Rectangle) -> Self {
-//         return RectIter {
-//             points: vec![
-//                 (rect.x, rect.y),
-//                 (rect.x + rect.width, rect.y),
-//                 (rect.x, rect.y + rect.height),
-//                 (rect.x + rect.width, rect.y + rect.height),
-//             ],
-//             idx: 0,
-//         };
-//     }
-// }
-
-impl IntoIterator for Rectangle {
-    type Item = (f64, f64);
-
-    type IntoIter = RectIter;
-
-    fn into_iter(self) -> Self::IntoIter {
-        // return RectIter::new(&self); // v1
-        return (&self).into(); // v2
-    }
-}
-
-impl IntoIterator for &Rectangle {
-    type Item = (f64, f64);
-
-    type IntoIter = RectIter;
-
-    fn into_iter(self) -> Self::IntoIter {
-        // return RectIter::new(self); // v1
-        return self.into(); // v2
     }
 }
